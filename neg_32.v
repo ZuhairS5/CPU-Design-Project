f06649980d2b8f85bb488e642ad54d5354aad65b
Zuhair Shaikh <73407270+ZuhairS5@users.noreply.github.com>
2023-03-14 00:41:31 -0400

    NEG Operation

--- neg_32.v
+++ neg_32.v
@@ -3,7 +3,6 @@
 // ELEC374 - Digital Systems Engineering
 // Department of Electrical and Computer Engineering
 // Queen's University
-
 `timescale 1ns/10ps
 
 module neg_32(
@@ -16,5 +15,5 @@ module neg_32(
 	
 	not_32 not_instance (rA, zTemp);
 	add_32 add_instance (zTemp, 32'd1, 1'd0, rZ, cOut);
-	
+
 endmodule 
