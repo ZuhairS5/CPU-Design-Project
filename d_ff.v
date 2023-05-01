e0d51bc7517fb21d51de1b0251830d43f0b60b07
Zuhair Shaikh <73407270+ZuhairS5@users.noreply.github.com>
2023-04-30 22:16:40 -0400

    D Flip-flop

---
+++ d_ff.v
@@ -0,0 +1,17 @@
+//// Zuhair Shaikh and Brant Lan Li
+// D Flip Flop
+// ELEC374 - Digital Systems Engineering
+// Department of Electrical and Computer Engineering
+// Queen's University 
+
+module d_ff(
+	input wire D,
+	input wire clk,
+	output reg Q
+	);
+	
+	always @ (posedge clk)
+		begin
+			Q <= D;
+		end
+endmodule
